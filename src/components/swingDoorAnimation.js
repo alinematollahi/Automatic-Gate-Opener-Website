868da9768b30f83c06051cfdfaecfd984a2fe584
Ali <ali.nematollahi2020@gmail.com>
2021-04-05 22:02:29 +0200

    define elements

--- src/components/swingDoorAnimation.js
+++ src/components/swingDoorAnimation.js
@@ -4,7 +4,22 @@ import '../styles/swingDoorAnimation.css'
 function SwingDoorAnimation(props) {
     return (
         <div id="swing-door-animation-container">
-        a
+            <div id="empty-space"></div>
+            <div id="wall-left-1"></div>
+            <div id="part-3">
+                <div id="back-base-left"></div>
+                <div id="wall-left-2"></div>
+            </div>
+            <div id="hinge-left"></div>
+            <div id="part-5">
+
+            </div>
+            <div id="hinge-right"></div>
+            <div id="part-7">
+                <div id="back-base-right"></div>
+                <div id="wall-right-2"></div>
+            </div>
+            <div id="wall-right-1"></div>
         </div>
     );
 }
