a23f73b1e021b11aaffc1a45688bbe6f7af04a3b
Ali <ali.nematollahi2020@gmail.com>
2021-04-05 19:53:35 +0200

    init to create animation for swing doors

---
+++ src/components/swingDoorAnimation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/swingDoorAnimation.css'
+
+function SwingDoorAnimation(props) {
+    return (
+        <div id="swing-door-animation-container">
+        a
+        </div>
+    );
+}
+
+export default SwingDoorAnimation;
