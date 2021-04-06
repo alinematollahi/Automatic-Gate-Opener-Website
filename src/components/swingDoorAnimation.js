9f7c76b6bffac92ef2470a808b1512fb8108527e
Ali <ali.nematollahi2020@gmail.com>
2021-04-06 16:24:45 +0200

    set jsx

--- src/components/swingDoorAnimation.js
+++ src/components/swingDoorAnimation.js
@@ -4,22 +4,44 @@ import '../styles/swingDoorAnimation.css'
 function SwingDoorAnimation(props) {
     return (
         <div id="swing-door-animation-container">
+
             <div id="empty-space"></div>
+
             <div id="wall-left-1"></div>
+
             <div id="part-3">
                 <div id="back-base-left"></div>
                 <div id="wall-left-2"></div>
             </div>
-            <div id="hinge-left"></div>
-            <div id="part-5">
 
+            <div id="hinge-container-left">
+                <div id="set-space-hinge-left"></div>
+                <div id="hinge-left"></div>
             </div>
-            <div id="hinge-right"></div>
+
+            <div id="part-5">
+                <div id="set-space-doors"></div>
+                <div id="door-left"></div>
+                <div id="edges">
+                    <div id="edge-left"></div>
+                    <div id="set-space-edges"></div>
+                    <div id="edge-right"></div>
+                </div>
+                <div id="door-right"></div>
+            </div>
+
+            <div id="hinge-container-right">
+                <div id="set-space-hinge-right"></div>
+                <div id="hinge-right"></div>
+            </div>
+
             <div id="part-7">
                 <div id="back-base-right"></div>
                 <div id="wall-right-2"></div>
             </div>
+
             <div id="wall-right-1"></div>
+            
         </div>
     );
 }
