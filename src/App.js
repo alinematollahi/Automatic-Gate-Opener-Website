4f4288e541c845cce57e1405bfc8d70496a1eb9a
Ali <ali.nematollahi2020@gmail.com>
2021-04-16 02:50:45 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
+import React from 'react';
 import './App.css';
-
+import  {HomePage}  from './pages/home';
+import  {ProductPage}  from './pages/products';
+import SwingDoorAnimation from './components/swingDoorAnimation';
 function App() {
   return (
-    ""
+    <div className="app">
+      <HomePage />
+      <ProductPage />
+      <SwingDoorAnimation />
+
+    </div>
   );
 }
 
