c8a3ca82147dffc46b925ba9a8b0f253a63e9455
Ali <ali.nematollahi2020@gmail.com>
2021-04-22 16:51:00 +0200

    update routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 
 import React from 'react';
 import './App.css';
-import  {HomePage}  from './pages/home';
-import  {ProductPage}  from './pages/products';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HomePage , ProductPage , DetailPage } from './pages';
+//import { ProductPage } from './pages/products';
 import SwingDoorAnimation from './components/swingDoorAnimation';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Navbar } from './components/navbar';
+//import { DetailPage } from './pages/detail';
+
 function App() {
   return (
     <div className="app">
-      <HomePage />
-      <ProductPage />
-      <SwingDoorAnimation />
-
+      <Navbar />
+      <div className="container">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/products" component={ProductPage} />
+            <Route path="/detail/:id" component={DetailPage} />
+            <Route path="/a" component={SwingDoorAnimation} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
