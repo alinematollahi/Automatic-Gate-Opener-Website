6f9005613e025d5180411e1be2538d7fbac676e1
Ali <ali.nematollahi2020@gmail.com>
2021-04-22 16:55:11 +0200

    set prduct page

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ProductList } from "../components/product";
+import { productService } from "../components/product";
 
-export function ProductPage(){
-    return(
-        <div>product page</div>
-    );
+
+
+export class ProductPage extends Component {
+    state = { products: [] };
+
+    async componentDidMount() {
+        const response = await productService.getProducts();
+        this.setState({ products: response.data });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <ProductList products={this.state.products} />
+                </div>
+            </div>
+
+        );
+    }
 };
