33b982b48b6d63cf695e4537b5ee91758400495f
Ali <ali.nematollahi2020@gmail.com>
2021-04-30 20:53:06 +0200

    add cart page

---
+++ src/pages/cart.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Cart } from '../components/cart';
+import { cartStore } from '../components/product';
+
+export class CartPage extends Component {
+    state = {}
+    /*const products = cartStore.getState(); */
+
+    componentDidMount() {
+        this.setState({ products: cartStore.getState() });
+        this.unsubscribe = cartStore.subscribe(()=>this.setState({ products: cartStore.getState() }));
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        return (
+            <div className="mt-5">
+                < Cart products={this.state.products} />
+            </div>
+        )
+    }
+}
