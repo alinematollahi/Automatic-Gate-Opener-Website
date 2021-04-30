5b63cc9e35c0d686bb12f84e20b838c9718736c8
Ali <ali.nematollahi2020@gmail.com>
2021-04-30 20:45:42 +0200

    update

--- src/components/product/redux/reducer.js
+++ src/components/product/redux/reducer.js
@@ -1,9 +1,21 @@
 import {ADD_TO_CART} from './actions';
+import {REMOVE_FROM_CART} from './actions';
 
 export function cartReducer(state=[],action){
     switch(action.type) {
         case ADD_TO_CART :
+            const product = state.find(item=>item.id === action.payload.id);
+            if(product){
+                product.quantity++;
+                const products = state.filter(item=>item.id !== action.payload.id);
+                return [...products, product]
+            }
+            action.payload.quantity=1;
             return [...state, action.payload]
+
+        case REMOVE_FROM_CART :
+            return state.filter(item=>item.id !== action.payload.id)
+
         default:
             return state;
     }
