8fcb66b8aaa5f7832ac1c64902e99d4623f0f1c9
Ali <ali.nematollahi2020@gmail.com>
2021-05-04 20:43:11 +0200

    add actions

--- src/components/product/redux/actions.js
+++ src/components/product/redux/actions.js
@@ -1,5 +1,7 @@
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const ADD_QUANTITY ='ADD_QUANTITY';
+export const REDUCE_QUANTITY ='REDUCE_QUANTITY';
 
 export function addToCart(product){
     return {
@@ -11,6 +13,20 @@ export function addToCart(product){
 export function removeFromCart(product){
     return {
         type: REMOVE_FROM_CART,
+        payload: product
+    }
+}
+
+export function addQuantity(product){
+    return {
+        type: ADD_QUANTITY,
+        payload: product
+    }
+}
+
+export function reduceQuantity(product){
+    return {
+        type: REDUCE_QUANTITY,
         payload: product
     }
 }
