edf108ccd91fd200e5864de219d7243755d324e5
Ali <ali.nematollahi2020@gmail.com>
2021-05-05 16:49:55 +0200

    add background image, title and link on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { ProductList } from "../components/product";
 import { productService } from "../components/product";
 
@@ -8,16 +9,35 @@ export class HomePage extends Component {
 
   state = { products: [] };
 
-  render () {
+  render() {
     return (
-        <div >
-          HomePage
+      <div>
+        <img style={{ width: '100vw' }} src="/pictures/bg.jpg" />
+        <div style={homePageStyle1}>
+          Automatic Gate Opener Website
+            </div>
+        <div style={homePageStyle2}>
+          <Link to="/products">See Products</Link>
         </div>
+
+      </div>
     );
   }
 
 };
 
+const homePageStyle1 = {
+  position: 'absolute',
+  top: 80,
+  left: 30,
+  fontSize: 35,
+  fontWeight: 700
+}
 
-  
-    
+const homePageStyle2 = {
+  position: 'absolute',
+  top: 130,
+  left: 30,
+  fontSize: 25,
+  fontWeight: 500
+}
