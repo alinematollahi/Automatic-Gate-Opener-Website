c270a58a2957ec597d181ce6464d7f2ba6673c14
Ali <ali.nematollahi2020@gmail.com>
2021-05-06 15:43:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HomePage, ProductPage, DetailPage, CartPage } from './pages';
-//import { ProductPage } from './pages/products';
-import SwingDoorAnimation from './components/swingDoorAnimation';
+import { HomePage, ProductPage, DetailPage, CartPage, TechnicalPage } from './pages';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Navbar } from './components/navbar';
 import { Footer } from './components/footer';
-//import { DetailPage } from './pages/detail';
+
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
             <Route exact path="/" component={HomePage} />
             <Route path="/products" component={ProductPage} />
             <Route path="/detail/:id" component={DetailPage} />
-            <Route path="/a" component={SwingDoorAnimation} />
+            <Route path="/technical" component={TechnicalPage} />
             <Route path="/cart" component={CartPage} />
           </Switch>
         </div>
