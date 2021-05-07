6c955f844dbd5cef46d1f2dcb79b5e6c65cb0c8f
Ali <ali.nematollahi2020@gmail.com>
2021-05-07 04:27:03 +0200

    update

--- src/components/product/redux/reducer.js
+++ src/components/product/redux/reducer.js
@@ -3,37 +3,48 @@ import { REMOVE_FROM_CART } from './actions';
 import { ADD_QUANTITY } from './actions';
 import { REDUCE_QUANTITY } from './actions';
 
-export function cartReducer(state = [], action) {
+var subtotal = 0 ;
+
+export function cartReducer(state = [subtotal], action) {
     switch (action.type) {
         case ADD_TO_CART:
             const product = state.find(item => item.id === action.payload.id);
             if (product) {
                 product.quantity++;
-                const products = state.filter(item => item.id !== action.payload.id);
-                return [...products, product]
+                subtotal = subtotal + action.payload.price;
+                const products = state.filter(item => item.id !== action.payload.id && item.id);
+                return [...products, product , subtotal]
             }
             action.payload.quantity = 1;
-            return [...state, action.payload]
+            subtotal = subtotal+ action.payload.price;
+            const products_add = state.filter(item =>item.id)
+            return [...products_add, action.payload , subtotal]
 
 
         case REMOVE_FROM_CART:
-            return state.filter(item => item.id !== action.payload.id)
+            subtotal = subtotal - (action.payload.price * action.payload.quantity);
+            const products_remove = state.filter(item => item.id !== action.payload.id && item.id);
+            return [...products_remove, subtotal]
 
 
         case ADD_QUANTITY:
             const theProduct = state.find(item => item.id === action.payload.id);
             theProduct.quantity++;
-            const otherProducts = state.filter(item => item.id !== action.payload.id);
-            return [...otherProducts, theProduct]
+            subtotal = subtotal + action.payload.price;
+            const otherProducts = state.filter(item => item.id !== action.payload.id && item.id);
+            return [...otherProducts, theProduct , subtotal]
 
         case REDUCE_QUANTITY:
             const theProduct_reduce_Quantity = state.find(item => item.id === action.payload.id);
             if(theProduct_reduce_Quantity.quantity==1){
-                return state.filter(item => item.id !== action.payload.id)
+                const products_reduce = state.filter(item => item.id !== action.payload.id && item.id);
+                subtotal = subtotal - action.payload.price;
+                return [...products_reduce, subtotal]
             }else{
                 theProduct_reduce_Quantity.quantity--;
-                const otherProducts_reduce_Quantity = state.filter(item => item.id !== action.payload.id);
-                return [...otherProducts_reduce_Quantity, theProduct_reduce_Quantity]
+                subtotal = subtotal - action.payload.price;
+                const otherProducts_reduce_Quantity = state.filter(item => item.id !== action.payload.id && item.id);
+                return [...otherProducts_reduce_Quantity, theProduct_reduce_Quantity , subtotal]
             }
         default:
             return state;
